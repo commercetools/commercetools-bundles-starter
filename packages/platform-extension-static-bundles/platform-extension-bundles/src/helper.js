07e54fece12a982e5703bd56d7ed30c93d260cdc
Nicholas Speeter <nicholas.speeter@commercetools.de>
2021-03-30 15:02:17 -0400

    refactor: addition of platform-extension-bundles and platform-extension-core

---
+++ packages/platform-extension-static-bundles/platform-extension-bundles/src/helper.js
@@ -0,0 +1,70 @@
+import { Utils } from '@commercetools-us-ps/platform-extension-core';
+
+export default ({
+  commercetools, cache, bundleProductTypeKey, customTypeKey
+}) => {
+  const utils = Utils({ cache });
+
+  const helper = {
+    utils,
+    uuid: utils.uuid,
+
+    // Memoize request URIs built
+    uri: {
+      bundleProductTypeByKey: commercetools
+        .getRequestBuilder()
+        .productTypes.byKey(bundleProductTypeKey)
+        .build(),
+      customTypeByKey: commercetools
+        .getRequestBuilder()
+        .types.byKey(customTypeKey)
+        .build(),
+    },
+  };
+
+  /**
+   * @returns {string} UUID of StaticBundleParent product type from the CT API
+   */
+  helper.getBundleProductTypeId = async () =>
+    utils.asyncMemoize('bundle_product_type_id', async () => {
+      const uri = helper.uri.bundleProductTypeByKey;
+      const res = await commercetools.client.get(uri);
+      const value = res?.body?.id;
+      if (!value) {
+        throw new Error(`No valid product type ID returned from ${uri}`);
+      }
+      return value;
+    });
+
+  /**
+   * @returns {string} UUID of StaticBundleParentChildLink type from the CT API
+   */
+  helper.getParentChildAssociationTypeId = async () =>
+    utils.asyncMemoize('parent_child_association_type_id', async () => {
+      const uri = helper.uri.customTypeByKey;
+      const res = await commercetools.client.get(uri);
+      const value = res?.body?.id;
+      if (!value) {
+        throw new Error(`No valid type ID returned from ${uri}`);
+      }
+      return value;
+    });
+
+  /**
+   * Returns a valid Static- or DynamicBundleParentChildLink custom type object.
+   */
+  helper.buildParentChildAssociationCustomType = ({ externalId, parent }) => {
+    const custom = {
+      type: {
+        key: customTypeKey,
+      },
+      fields: {
+        'external-id': externalId,
+      },
+    };
+    if (parent) custom.fields.parent = parent;
+    return custom;
+  };
+
+  return helper;
+};
