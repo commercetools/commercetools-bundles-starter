30c6239ceda90565a5cc8b2614939280162671dd
Nicholas Speeter <nicholas.speeter@commercetools.de>
2021-03-30 11:01:31 -0400

    fix: lint static bundles

--- packages/bundles-static/src/components/bundles-table/static-bundles-table.js
+++ packages/bundles-static/src/components/bundles-table/static-bundles-table.js
@@ -4,11 +4,11 @@ import { FormattedDate, FormattedNumber, useIntl } from 'react-intl';
 import { find, minBy } from 'lodash';
 import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
 import { NO_VALUE_FALLBACK } from '@commercetools-frontend/constants';
-import { localize } from '../../../../bundles-core/components/generic/mc-custom-applications-core/src/util';
 import {
   BundlesTable,
   COLUMN_KEYS,
 } from '@commercetools-us-ps-local/bundles-core/components';
+import { localize } from '../../../../bundles-core/components/generic/mc-custom-applications-core/src/util';
 import {
   CategorySearchInput,
   ProductSearchInput,
