11deb997883a417b8afb8f0f8c60052fb2f0e3f3
praveenkumarct <praveen.kumar@commercetools.com>
2021-10-27 12:38:51 +0200

    change file extension to js.

--- packages/platform-extension-static-bundles/platform-extension-core/src/index.js
+++ packages/platform-extension-static-bundles/platform-extension-core/src/index.js
@@ -1,4 +1,4 @@
-export * from './api-extension';
+export * from './api-extension.js';
 export { default as Commercetools } from './commercetools.js';
 export * from './commercetools.js';
 export { default as Utils } from './utils.js';
