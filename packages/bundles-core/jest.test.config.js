6241b10c34de1a6940e332ed81f77a116fe4ffb8
aoz <bilmuhahmet@gmail.com>
2021-11-01 15:28:04 +0100

    update jest-preset and use applyJestPresetWithEnzyme.

--- packages/bundles-core/jest.test.config.js
+++ packages/bundles-core/jest.test.config.js
@@ -1,6 +1,15 @@
+const jestPreset = require('@commercetools-frontend/jest-preset-mc-app');
+const applyJestPresetWithEnzyme = require('@commercetools-frontend/jest-preset-mc-app/enzyme/apply-jest-preset-with-enzyme');
+
 module.exports = {
-  preset: '@commercetools-frontend/jest-preset-mc-app',
+  ...applyJestPresetWithEnzyme({
+    enzymeAdapterVersion: 17,
+    jestPreset,
+  }),
+  moduleDirectories: ['src', 'node_modules'],
   transformIgnorePatterns: [
     '/node_modules/(?!intl-messageformat|intl-messageformat-parser).+\\.js$',
   ],
+  collectCoverage: true,
 };
+
