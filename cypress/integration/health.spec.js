51d2d226789019bf65acec62e421f73b80edc858
praveenkumarct <praveen.kumar@commercetools.com>
2021-12-20 13:33:00 +0100

    Update config.

--- cypress/integration/health.spec.js
+++ cypress/integration/health.spec.js
@@ -2,10 +2,9 @@ import { entryPointUriPath } from '../support/constants';
 
 describe('Health', () => {
   const initialUri = 'bundles-test';
-  const dotfiles = [".env"];
 
   it(`should open the accounts url and show the profile`, () => {
-    cy.loginByOidc({ entryPointUriPath, dotfiles });
+    cy.loginByOidc({ entryPointUriPath });
 
     cy.url().should('include', 'bundles-test');
 
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,35 @@
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @param `on` is used to hook into various events Cypress emits
+ * @param `config` is the resolved Cypress config
+ * @return {[type]} [description]
+ */
+
+// Reference: https://docs.cypress.io/api/plugins/configuration-api.html#Promises
+
 const customApplications = require('@commercetools-frontend/cypress/task');
 const failedLog = require('cypress-failed-log/src/failed');
 
 // plugins file
 module.exports = (on, cypressConfig) => {
+    // Load the config
+    if (!process.env.CI) {
+        const path = require('path');
+        const envPath = path.join(__dirname, '../.env');
+        require('dotenv').config({ path: envPath });
+    }
 
     on('task', {
         failed: failedLog(),
@@ -10,10 +37,13 @@ module.exports = (on, cypressConfig) => {
     });
 
     return {
+        ...cypressConfig,
         env: {
-            LOGIN_USER: 'praveen.kumar@commercetools.com',
-            LOGIN_PASSWORD: '',
-            PROJECT_KEY: 'bundles-test',
+            ...cypressConfig.env,
+            LOGIN_USER: "praveen.kumar@commercetools.com",
+            LOGIN_PASSWORD: "",
+            PROJECT_KEY: "bundles-test"
         },
     };
 };
+
