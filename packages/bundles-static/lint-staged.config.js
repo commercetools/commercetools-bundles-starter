a0f4911285c0d3721a5c286b2a4c5f8f3f491303
praveenkumarct <praveen.kumar@commercetools.com>
2022-11-07 04:08:43 +0100

    test lint staged precommit.

--- packages/bundles-static/lint-staged.config.js
+++ packages/bundles-static/lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '*.js': [
     'yarn lint:js --reporters=jest-silent-reporter --onlyChanged',
     'yarn format:js',
-  ],
+  ], // TODO: test
   '*.css': [
     'yarn lint:css -- --reporters=jest-silent-reporter --onlyChanged',
     'yarn format:css',
